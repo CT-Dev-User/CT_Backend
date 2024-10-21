6401fc7e3fd22c40d57dcccfa8e79e97d7f6a8d8
CT-Dev-User <projects@conscientioustech.in>
2024-10-21 14:56:59 +0530

    changes at Mondat

--- latestModels/blogs/Blogs.js
+++ latestModels/blogs/Blogs.js
@@ -1,20 +1,19 @@
+//latestModel/blog/blogs.js
 import mongoose from "mongoose";
-
 const LatestBlogSchema = new mongoose.Schema({
     category: { type: String },
     subCategory: { type: String },
-    cardData: {
-        heading: { type: String },
-        subHeading: { type: String },
-        CardImg: { type: String },
-        headerImg: { type: String },
-    },
+    headerTitle:{type:String},
+    HeaderDesc:{type:String},
+    headerImage: { type: String },
+    cardHeading: { type: String },
+    cardSubHeading: { type: String },
+    cardImage: { type: String },
     articleData: [{
         Title: { type: String },
         Desc: { type: String }
     }],
     blogTags: { type: String } // separated by commas ", "
 }, { timestamps: true });
-
 const LatestBlogModel = mongoose.model('LatestBlog', LatestBlogSchema);
 export default LatestBlogModel;
